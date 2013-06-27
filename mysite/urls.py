2031c2edaba4d41bafbf7470898f53f672381dc0
unknown <gecortizo@UW-000248.uniway.local>
2013-06-27 17:39:43 +0200

    commit 27/06/2013 desde aptan: Modificacion modelo

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,20 @@
 # -*- coding: cp1252 -*-
-from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
 from views import *
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 # USE (nombreWeb, nombreMethod)
-urlpatterns = patterns	('', 
-						('^hello/$', hello), 
-						('^clock/$', clock),
-						('^login/$', loginform), 
-						('^actions/$', loginMade), 						)
+urlpatterns = patterns  ('', 
+                           ('^$',index_view),
+                           ('^inicio/$',index_view),
+                           ('^login/$',login_view), 
+                           ('^logout/$',logout_view),
+                           ('^search/$',search_view),
+                           ('^contact/$',contact_view),
+                           ('^register/$',register_view),
+                           ('^perfil/$',perfil_view),
+                           ('^admin/', include(admin.site.urls)),
+                           )
